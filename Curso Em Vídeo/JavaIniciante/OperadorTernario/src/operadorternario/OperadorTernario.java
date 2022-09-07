d46de4b742bd77eed3b8df96e20223b21f125e3c
Leonardo César <leocesar1977@Gmail.com>
2022-09-07 14:59:42 -0300

    Atualização Aula 07

---
+++ Curso Em Vídeo/JavaIniciante/OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,13 @@
+package operadorternario;
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+        int n1, n2, result;
+        n1 = 16;
+        n2 = 8;
+        result = (n1 > n2) ? 0 : 1;
+        System.out.println("result: " + result);
+    }
+    
+}
